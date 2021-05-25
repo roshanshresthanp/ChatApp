d3ad3b597eaca728b47288a402f06e8e22a63f81
roshan <stha901@gmail.com>
2021-05-26 01:11:24 +0545

    changes made to route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ChatController;
+use App\Http\Controllers\ChatController; 
 
 /*
 |--------------------------------------------------------------------------
